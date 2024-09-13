28db5421ef3605f6269bcee8284f3cc56e9d1ad4
Kyriann <kyriann.paille@gmail.com>
2024-09-13 09:30:23 +0200

    Initial Commit

---
+++ wombini/wombiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  wombiniApp.swift
+//  wombini
+//
+//  Created by Kyriann Paille on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct wombiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
