44e3753173275d6d96f5c0d75c3c39f9121f707c
Malfioso <jeremsxm97@gmail.com>
2024-10-18 14:58:49 +0200

    fixing the tests

--- wombiniUITests/wombiniUITests.swift
+++ wombiniUITests/wombiniUITests.swift
@@ -1,13 +1,21 @@
 import XCTest
 
-class PopularAnimeViewUITests: XCTestCase {
-
+class AnimePopularViewUITests: XCTestCase {
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-    }
+            continueAfterFailure = false
 
+            // Launch the application
+            let app = XCUIApplication()
+            app.launch()
+
+            // Ensure the device is in portrait orientation
+            let device = XCUIDevice.shared
+            if device.orientation != .portrait {
+                device.orientation = .portrait
+            }
+        }
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -15,26 +23,21 @@ class PopularAnimeViewUITests: XCTestCase {
 
     func testWaitForManualTapOnAnime() throws {
         let app = XCUIApplication()
-        app.launch()
-
+                app.launch()
+                
+        
         // Perform a search
         let searchField = app.textFields["SearchAnimeField"]
         XCTAssertTrue(searchField.exists, "The search field should be present")
-        
+
         // Input search text (e.g., "Dandadan")
         searchField.tap()
         searchField.typeText("Dandadan")
-        
-        // Add a custom pause to allow manual interaction
-        print("Manually tap on the anime in the UI now.")
-        sleep(10)  // Pauses the test for 10 seconds, allowing time for manual tap
-        
+        XCUIApplication().scrollViews.containing(.other, identifier:"Vertical scroll bar, 2 pages").children(matching: .other).element(boundBy: 0).children(matching: .other).element.tap()
+
         // Check if the detail view is showing the correct anime title after manual tap
-        let detailTitle = app.staticTexts["Dandadan"]  // Replace "Dandadan" with the actual title in your detail view
-        let exists = NSPredicate(format: "exists == 1")
+                let detailTitle = app.staticTexts["Dandadan"]  // Replace "Dandadan" with the actual title in your detail view
         
-        expectation(for: exists, evaluatedWith: detailTitle, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
 
         XCTAssertTrue(detailTitle.exists, "The detail view for the anime 'Dandadan' should be displayed")
     }
